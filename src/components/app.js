af94d205401f70c3fddf3f5e570ebe6cb555e518
Simon Luu <luus2016@gmail.com>
2017-01-06 01:05:57 -0800

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react';
 import Hearthstone from './hearthstone';
 
 export default class App extends Component {
+	mobileAndTabletcheck() {
+		let check;
+		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			return true;
+		} else {
+			return false;
+		}
+		return check;
+	}
+
 	render() {
+		if (this.mobileAndTabletcheck()) {
+			return (
+				<div className="loading">
+					<div className="loading-div">
+						Please use a Desktop
+					</div>
+				</div>
+			);
+		}
+
 		return (
 			<div className="main_application">
 				<Hearthstone />
