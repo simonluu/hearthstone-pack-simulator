b1304f7af7fd67369626a5d85938116b4b4b2a81
Simon Luu <luus2016@gmail.com>
2017-01-06 01:11:44 -0800

    removed github icon

--- src/components/app.js
+++ src/components/app.js
@@ -27,9 +27,6 @@ export default class App extends Component {
 		return (
 			<div className="main_application">
 				<Hearthstone />
-				<a className="github" href="https://github.com/hearthstonepacksimulator/hearthstonepackstimulator.github.io">
-					<i className="fa fa-github-square" aria-hidden="true" style={{ marginRight : '5px', color : 'black' }}></i>
-				</a>
 			</div>
 		);
 	}
